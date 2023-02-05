fb4f77bf19632bc911c5ca4f7b3d7ea09e08ef65
Josh Anness <annessjosh@gmail.com>
2023-02-05 18:11:41 -0500

    Update / endpoint

--- src/main/java/com/plantplaces/PlantPlacesController.java
+++ src/main/java/com/plantplaces/PlantPlacesController.java
@@ -15,6 +15,10 @@ public class PlantPlacesController {
 		return "start";
 	}
 	
+	/**
+	 * Handle the / endpoint
+	 * @return
+	 */
 	@RequestMapping("/")
 	public String index() {
 		return "start";
